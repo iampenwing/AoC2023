2cd0226a0eebcb975f80b9e6093191c4cb58736c
penwing <alex@penwing.me.uk>
2023-12-04 11:46:43 +0000

    Added getInts function

--- AoCLib/AoCLib.hs
+++ AoCLib/AoCLib.hs
@@ -12,7 +12,8 @@ module AoCLib.AoCLib
     getLast,
     getDigits,
     getFirstDigit,
-    getLastDigit
+    getLastDigit,
+    getInts
   ) where
 
 import qualified Data.Set as Set
@@ -176,4 +177,51 @@ getLastDigit_ (x:xs)
   | (xLen >= 4) && (x == 'e') && ((head xs) == 'n') && ((head (tail xs)) == 'i') && ((head (tail (tail xs))) == 'n') = 9
   | otherwise = getLastDigit_ xs
   where xLen = length (x:xs)
+
+getInts :: String -> [Int]
+getInts [] = []
+getInts (x:xs)
+  | x == '1' = getInts_inNumber xs 1 []
+  | x == '2' = getInts_inNumber xs 2 []
+  | x == '3' = getInts_inNumber xs 3 []
+  | x == '4' = getInts_inNumber xs 4 []
+  | x == '5' = getInts_inNumber xs 5 []
+  | x == '6' = getInts_inNumber xs 6 []
+  | x == '7' = getInts_inNumber xs 7 []
+  | x == '8' = getInts_inNumber xs 8 []
+  | x == '9' = getInts_inNumber xs 9 []
+  | x == '0' = getInts_inNumber xs 0 []
+  | otherwise = getInts_rubbish xs []
+
+getInts_inNumber :: String -> Int -> [Int] -> [Int]
+getInts_inNumber [] n acc = (n:acc)
+getInts_inNumber (x:xs) n acc
+  | x == '1' = getInts_inNumber xs ((n * 10) + 1) acc
+  | x == '2' = getInts_inNumber xs ((n * 10) + 2) acc
+  | x == '3' = getInts_inNumber xs ((n * 10) + 3) acc
+  | x == '4' = getInts_inNumber xs ((n * 10) + 4) acc
+  | x == '5' = getInts_inNumber xs ((n * 10) + 5) acc
+  | x == '6' = getInts_inNumber xs ((n * 10) + 6) acc
+  | x == '7' = getInts_inNumber xs ((n * 10) + 7) acc
+  | x == '8' = getInts_inNumber xs ((n * 10) + 8) acc
+  | x == '9' = getInts_inNumber xs ((n * 10) + 9) acc
+  | x == '0' = getInts_inNumber xs ((n * 10) + 0) acc
+  | otherwise = getInts_rubbish xs (n:acc)
+
+getInts_rubbish :: String -> [Int] -> [Int]
+getInts_rubbish [] acc = acc
+getInts_rubbish (x:xs) acc
+  | x == '1' = getInts_inNumber xs 1 acc
+  | x == '2' = getInts_inNumber xs 2 acc
+  | x == '3' = getInts_inNumber xs 3 acc
+  | x == '4' = getInts_inNumber xs 4 acc
+  | x == '5' = getInts_inNumber xs 5 acc
+  | x == '6' = getInts_inNumber xs 6 acc
+  | x == '7' = getInts_inNumber xs 7 acc
+  | x == '8' = getInts_inNumber xs 8 acc
+  | x == '9' = getInts_inNumber xs 9 acc
+  | x == '0' = getInts_inNumber xs 0 acc
+  | otherwise = getInts_rubbish xs acc
+
+  
 
