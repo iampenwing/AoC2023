a0a84b832e6bf7db2340c69ba40c607963dfa286
penwing <alex@penwing.me.uk>
2023-12-09 10:41:03 +0000

    Adding negative numbers to AoCLib.getInts for Day 9

--- AoCLib/AoCLib.hs
+++ AoCLib/AoCLib.hs
@@ -178,22 +178,47 @@ getLastDigit_ (x:xs)
   | otherwise = getLastDigit_ xs
   where xLen = length (x:xs)
 
-getInts :: String -> [Int]
+getInts :: String -> [Integer]
 getInts [] = []
-getInts (x:xs)
-  | x == '1' = getInts_inNumber xs 1 []
-  | x == '2' = getInts_inNumber xs 2 []
-  | x == '3' = getInts_inNumber xs 3 []
-  | x == '4' = getInts_inNumber xs 4 []
-  | x == '5' = getInts_inNumber xs 5 []
-  | x == '6' = getInts_inNumber xs 6 []
-  | x == '7' = getInts_inNumber xs 7 []
-  | x == '8' = getInts_inNumber xs 8 []
-  | x == '9' = getInts_inNumber xs 9 []
-  | x == '0' = getInts_inNumber xs 0 []
-  | otherwise = getInts_rubbish xs []
+getInts (x:(y:xs))
+  | x == '1' = getInts_inNumber (y:xs) 1 []
+  | x == '2' = getInts_inNumber (y:xs) 2 []
+  | x == '3' = getInts_inNumber (y:xs) 3 []
+  | x == '4' = getInts_inNumber (y:xs) 4 []
+  | x == '5' = getInts_inNumber (y:xs) 5 []
+  | x == '6' = getInts_inNumber (y:xs) 6 []
+  | x == '7' = getInts_inNumber (y:xs) 7 []
+  | x == '8' = getInts_inNumber (y:xs) 8 []
+  | x == '9' = getInts_inNumber (y:xs) 9 []
+  | x == '0' = getInts_inNumber (y:xs) 0 []
+  | x == '-' && y == '1' = getInts_inNumberNegative xs 1 []
+  | x == '-' && y == '2' = getInts_inNumberNegative xs 2 []
+  | x == '-' && y == '3' = getInts_inNumberNegative xs 3 []
+  | x == '-' && y == '4' = getInts_inNumberNegative xs 4 []
+  | x == '-' && y == '5' = getInts_inNumberNegative xs 5 []
+  | x == '-' && y == '6' = getInts_inNumberNegative xs 6 []
+  | x == '-' && y == '7' = getInts_inNumberNegative xs 7 []
+  | x == '-' && y == '8' = getInts_inNumberNegative xs 8 []
+  | x == '-' && y == '9' = getInts_inNumberNegative xs 9 []
+  | x == '-' && y == '0' = getInts_inNumberNegative xs 0 []
+  | otherwise = getInts_rubbish (y:xs) []
 
-getInts_inNumber :: String -> Int -> [Int] -> [Int]
+getInts_inNumberNegative :: String -> Integer -> [Integer] -> [Integer]
+getInts_inNumberNegative [] n acc = (((-1) * n):acc)
+getInts_inNumberNegative (x:xs) n acc
+  | x == '1' = getInts_inNumberNegative xs ((n * 10) + 1) acc
+  | x == '2' = getInts_inNumberNegative xs ((n * 10) + 2) acc
+  | x == '3' = getInts_inNumberNegative xs ((n * 10) + 3) acc
+  | x == '4' = getInts_inNumberNegative xs ((n * 10) + 4) acc
+  | x == '5' = getInts_inNumberNegative xs ((n * 10) + 5) acc
+  | x == '6' = getInts_inNumberNegative xs ((n * 10) + 6) acc
+  | x == '7' = getInts_inNumberNegative xs ((n * 10) + 7) acc
+  | x == '8' = getInts_inNumberNegative xs ((n * 10) + 8) acc
+  | x == '9' = getInts_inNumberNegative xs ((n * 10) + 9) acc
+  | x == '0' = getInts_inNumberNegative xs ((n * 10) + 0) acc
+  | otherwise = getInts_rubbish xs (((-1) * n):acc)
+
+getInts_inNumber :: String -> Integer -> [Integer] -> [Integer]
 getInts_inNumber [] n acc = (n:acc)
 getInts_inNumber (x:xs) n acc
   | x == '1' = getInts_inNumber xs ((n * 10) + 1) acc
@@ -208,20 +233,30 @@ getInts_inNumber (x:xs) n acc
   | x == '0' = getInts_inNumber xs ((n * 10) + 0) acc
   | otherwise = getInts_rubbish xs (n:acc)
 
-getInts_rubbish :: String -> [Int] -> [Int]
+getInts_rubbish :: String -> [Integer] -> [Integer]
 getInts_rubbish [] acc = acc
-getInts_rubbish (x:xs) acc
-  | x == '1' = getInts_inNumber xs 1 acc
-  | x == '2' = getInts_inNumber xs 2 acc
-  | x == '3' = getInts_inNumber xs 3 acc
-  | x == '4' = getInts_inNumber xs 4 acc
-  | x == '5' = getInts_inNumber xs 5 acc
-  | x == '6' = getInts_inNumber xs 6 acc
-  | x == '7' = getInts_inNumber xs 7 acc
-  | x == '8' = getInts_inNumber xs 8 acc
-  | x == '9' = getInts_inNumber xs 9 acc
-  | x == '0' = getInts_inNumber xs 0 acc
-  | otherwise = getInts_rubbish xs acc
+getInts_rubbish (x:(y:xs)) acc
+  | x == '1' = getInts_inNumber (y:xs) 1 acc
+  | x == '2' = getInts_inNumber (y:xs) 2 acc
+  | x == '3' = getInts_inNumber (y:xs) 3 acc
+  | x == '4' = getInts_inNumber (y:xs) 4 acc
+  | x == '5' = getInts_inNumber (y:xs) 5 acc
+  | x == '6' = getInts_inNumber (y:xs) 6 acc
+  | x == '7' = getInts_inNumber (y:xs) 7 acc
+  | x == '8' = getInts_inNumber (y:xs) 8 acc
+  | x == '9' = getInts_inNumber (y:xs) 9 acc
+  | x == '0' = getInts_inNumber (y:xs) 0 acc
+  | x == '-' && y == '1' = getInts_inNumberNegative xs 1 acc
+  | x == '-' && y == '2' = getInts_inNumberNegative xs 2 acc
+  | x == '-' && y == '3' = getInts_inNumberNegative xs 3 acc
+  | x == '-' && y == '4' = getInts_inNumberNegative xs 4 acc
+  | x == '-' && y == '5' = getInts_inNumberNegative xs 5 acc
+  | x == '-' && y == '6' = getInts_inNumberNegative xs 6 acc
+  | x == '-' && y == '7' = getInts_inNumberNegative xs 7 acc
+  | x == '-' && y == '8' = getInts_inNumberNegative xs 8 acc
+  | x == '-' && y == '9' = getInts_inNumberNegative xs 9 acc
+  | x == '-' && y == '0' = getInts_inNumberNegative xs 0 acc
+  | otherwise = getInts_rubbish (y:xs) acc
 
   
 
